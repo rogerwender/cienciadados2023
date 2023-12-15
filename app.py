281734b8d5016d84d12c8e8afa00be87d6bcd0fc
Rogerio Wender Tavares <f8720558@bb.com.br>
2023-12-14 22:29:59 -0300

    aaa

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Minha página de portfólioaaaa!'
+    return render_template('index.html')
+    
+    # return 'Minha página de portfólioaaaa!'
