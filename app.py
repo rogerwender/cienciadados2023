d60cf41c0a29f926a5c2dc46b3969494092ed7b3
Rogerio Wender Tavares <f8720558@bb.com.br>
2023-12-14 22:50:09 -0300

    afdf

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
     
     # return 'Minha página de portfólioaaaa!'
+
+if __name__ == "__main__":
+    app.run()
