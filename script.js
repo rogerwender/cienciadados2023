caaed6f396d2235d3e4150fcf4414d667a9eb5ec
Rogerio Wender Tavares <f8720558@bb.com.br>
2023-12-15 23:14:31 -0300

    ss

--- script.js
+++ script.js
@@ -75,3 +75,16 @@ function filtrarPorCategoria(filtro){
         }
     }
 }
+
+//do menu lateral
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
